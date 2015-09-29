b1f28e5c22e81fb2dfdb7869cbbe1e7de9a906ba
Jacky <kuoyichun1102@gmail.com>
2015-09-29 23:34:24 +0800

    implement cd command

---
+++ assignment1/cd.c
@@ -0,0 +1,24 @@
+/*************************************************************************
+	> File Name: cd.c
+	> Author: jacky
+	> Mail: kuoyichun1102@gmail.com
+	> Created Time: 二  9/29 21:56:21 2015
+ ************************************************************************/
+
+#include<unistd.h>
+#include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include"cd.h"
+
+void cd(char* dest){
+    dest[strlen(dest)-1] = '\0';
+    if(chdir(dest) != -1){
+        return;
+    }else{
+        printf("[%s]: cannot change directory\n", dest);
+        printf("Error: [%s]\n",strerror(errno));
+        return;
+    }
+    
+}
--- assignment1/shell.c
+++ assignment1/shell.c
@@ -5,5 +5,42 @@
 	> Created Time: 二  9/29 20:29:38 2015
  ************************************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
+#include "cd.h"
 
+
+
+
+int main(void){
+    char buf[255];
+    char path[255];
+    while(1){
+        printf("[3150 shell:%s]$ ", getcwd(path, PATH_MAX+1));
+        fgets(buf, 255, stdin);
+
+        //tokenize command
+        char* token = strtok(buf, " ");
+       
+        if(strcmp(token, "cd") == 0){
+            token = strtok(NULL, " ");
+            if(strtok(NULL, " ")!=NULL){
+                printf("cd: wrong number of arguments\n");
+            }else{
+                cd(token);
+            }
+        }else if(strcmp(token, "exit") == 0){
+            
+        }else if(strcmp(token, "fg") == 0){
+
+        }else if(strcmp(token, "jobs") == 0){
+
+        }else{
+
+        }
+    }
+    return 0;
+}
