b4203ccd8e9b5e1e1b2ca6061257f9923a8b7edb
jacky <kuoyichun1102@gmail.com>
2015-11-29 18:08:00 +0800

    Milestone 2, root dir and subdir

--- assignment2/readDir.c
+++ assignment2/readDir.c
@@ -13,26 +13,39 @@ void printFileName(char* filename){
 int main(int argc, char **argv){
 	FILE* file;
 	char buf[1000];
-	struct BootEntry* be;
+	struct BootEntry be;
 	file = fopen(argv[1], "r");
 	if(file == NULL){
 		perror(argv[1]);
 		exit(1);
 	}
 	fread(buf, sizeof(struct BootEntry), 1, file);
-	be = (struct BootEntry*)buf;
-	struct DirEntry* dirEntry;
-	long pos = be->BPB_BytsPerSec*(be->BPB_RsvdSecCnt+
-					be->BPB_NumFATs*be->BPB_FATSz32+
-					(be->BPB_RootClus-2)*be->BPB_SecPerClus);
+	memcpy(&be, buf, sizeof(struct BootEntry));
+	struct DirEntry dirEntry;
+	long pos = be.BPB_BytsPerSec*(be.BPB_RsvdSecCnt+
+					be.BPB_NumFATs*be.BPB_FATSz32+
+					(be.BPB_RootClus-2)*be.BPB_SecPerClus);
 	fseek(file, pos, SEEK_SET);
 	while(1){
 		fread(buf, sizeof(struct DirEntry), 1, file);
 		if(buf[0] == '\0') break;
-		dirEntry = (struct DirEntry*)buf;
-		printFileName(dirEntry->DIR_Name);
-		printf(" size: %ld", dirEntry->DIR_FileSize);
-		printf("%d %d\n", dirEntry->DIR_FstClusHI, dirEntry->DIR_FstClusLO);
+		memcpy(&dirEntry, buf, sizeof(struct DirEntry));
+		printFileName(dirEntry.DIR_Name);
+		printf(" size: %ld, ", dirEntry.DIR_FileSize);
+		printf("First Cluster address: %02x%02x\n", dirEntry.DIR_FstClusHI, dirEntry.DIR_FstClusLO);
+		long clus = dirEntry.DIR_FstClusHI*256+dirEntry.DIR_FstClusLO;//strtol(hex, NULL, 0);
+		long backPos = ftell(file);
+		fseek(file, be.BPB_BytsPerSec*(be.BPB_RsvdSecCnt+
+					be.BPB_NumFATs*be.BPB_FATSz32+
+					(clus-2)*be.BPB_SecPerClus), SEEK_SET);
+		char tmp;
+		while(1){
+			fread(&tmp, 1, 1, file);
+			if(tmp == '\0') break;
+			printf("%c",tmp);
+		}
+		printf("\n");
+		fseek(file, backPos, SEEK_SET);
 	}
 	return 0;
 }
--- assignment2/recover.c
+++ assignment2/recover.c
@@ -2,30 +2,122 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include "struct.h"
+#define ENTRY_PER_DIR ((bootSector.BPB_BytsPerSec*bootSector.BPB_SecPerClus)/32)
+FILE* file;
+char bbuf[1024];
+struct BootEntry bootSector;
+struct DirEntry dirEntry;
+long dataAreaPosi;
+
 void usage(char* arg){
 	printf("Usage: %s -d [device filename] [other arguments]\n", arg);
 	printf("-l target            List the target directory\n");
 	printf("-r target -o dest    Recoverthe target pathname\n");
 }
+
+
+void printFileName(char* filename){
+	int i;
+	for(i = 0; i < 8 && filename[i] != ' '; i++) printf("%c",filename[i]);
+	if(filename[8] == ' ') return;
+	printf(".");
+	for(i = 8; i < 11 && filename[i] != ' '; i++) printf("%c", filename[i]);
+}
+
+/* Set file stream indicator to the beginning of nth clus*/ 
+void setToNthClus(int n){
+	fseek(file, dataAreaPosi+bootSector.BPB_BytsPerSec*bootSector.BPB_SecPerClus*(n-2), SEEK_SET);
+}
+
+int nextClus(int curClus){
+	long backupPos = ftell(file);
+	long pos = bootSector.BPB_BytsPerSec*(bootSector.BPB_RsvdSecCnt)+sizeof(int)*curClus;
+	fseek(file, pos, SEEK_SET);
+	int next; 
+	fread(&next, sizeof(int), 1, file);
+	fseek(file, backupPos, SEEK_SET);
+	return next;	 
+}
+
+int findFirstClus(struct DirEntry curDirEntry){
+	return curDirEntry.DIR_FstClusHI*256+curDirEntry.DIR_FstClusLO; 
+}
+
+long findDirClus(long curDirClus, char* target){
+	long backupPos = ftell(file);
+	char buf[ENTRY_PER_DIR][sizeof(struct DirEntry)];
+/* A directory may contains more than one clustor*/
+	long targetDirClus;
+	while(curDirClus != 0){
+		setToNthClus(curDirClus);
+		fread(buf, sizeof(struct DirEntry), ENTRY_PER_DIR, file);
+		int i;
+		for(i = 0; i < ENTRY_PER_DIR; i++){
+			struct DirEntry *tmp = (struct DirEntry*)buf[i];
+			if(memcmp(target, tmp->DIR_Name, strlen(target)) == 0){
+				targetDirClus = findFirstClus(*tmp);
+				goto restore;
+			}
+		}	
+		curDirClus = nextClus(curDirClus);
+	}
+restore:
+	fseek(file, backupPos, SEEK_SET);		
+	return targetDirClus; 
+}
+
+void listDirEntry(char* targetDir){
+	long backupPos = ftell(file);
+	char *tmp = strtok(targetDir, "/");
+	long curDirClus = bootSector.BPB_RootClus;
+	while(tmp != NULL){
+		curDirClus = findDirClus(curDirClus, tmp);
+		tmp = strtok(NULL, "/");
+	}
+
+	char buf[ENTRY_PER_DIR][sizeof(struct DirEntry)];
+/* A directory may contains more than one clustor*/
+	int i = 1;
+	while(curDirClus != 0){
+		setToNthClus(curDirClus);
+		fread(buf, sizeof(struct DirEntry), ENTRY_PER_DIR, file);
+		int j;
+		for(j = 0; j < ENTRY_PER_DIR && buf[j][0] != '\0'; j++, i++){
+			struct DirEntry *tmp = (struct DirEntry*)buf[j];
+			printf("%d, ", i);
+			printFileName(tmp->DIR_Name);
+			if(tmp->DIR_Attr & 16) printf("/");
+			printf(", %ld, %d\n", tmp->DIR_FileSize, findFirstClus(*tmp));
+		}	
+		curDirClus = nextClus(curDirClus);
+	}
+	fseek(file, backupPos, SEEK_SET);		
+}
+
 int main(int argc, char** argv){
-	int dFlag = 0, ch, fd;
-	char *device, *target, *dest; 
+	int ch, fd;
+	char *device = NULL, *targetDir = NULL, *targetFile = NULL, *dest = NULL; 
 	if(argc == 1){
 		usage(argv[0]);	
 		exit(0);
 	}
+
+	/* Handle option argument*/
 	while((ch = getopt(argc, argv, "d:l:r:o")) != -1){
 		switch(ch){
 			case 'd':
-				dFlag = 1;
 				device = malloc(strlen(optarg));
 				strcpy(device, optarg);
 				break;
 			case 'l':
-				target = malloc(strlen(optarg));
-				strcpy(target, optarg);
+				targetDir = malloc(strlen(optarg));
+				strcpy(targetDir, optarg);
 				break;
 			case 'r':
+				targetFile = malloc(strlen(optarg));
+				strcpy(targetFile, optarg);
+			case 'o':
 				dest = malloc(strlen(optarg));
 				strcpy(dest, optarg);
 				break;
@@ -35,8 +127,20 @@ int main(int argc, char** argv){
 				exit(0);
 		}
 	}
-
-	if(!dFlag) usage(argv[0]);
+	if(device == NULL) usage(argv[0]);
+	
+	file = fopen(device, "r");
+	if(file == NULL){
+		perror(device);
+		exit(1);
+	}
+	
+	fread(bbuf, sizeof(struct BootEntry), 1, file);
+	memcpy(&bootSector, bbuf, sizeof(struct BootEntry));
+	dataAreaPosi = bootSector.BPB_BytsPerSec*(bootSector.BPB_RsvdSecCnt+bootSector.BPB_NumFATs*bootSector.BPB_FATSz32);
+	if(targetDir != NULL){
+		listDirEntry(targetDir);
+	}
 	return 0;	 
 }
 
